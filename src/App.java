065283ff018f26c92c9cef7f174cb298606df979
danielcbustos <d.bustosr@uniandes.edu.co>
2023-08-07 17:45:55 -0500

    Calculadora simple

--- src/App.java
+++ src/App.java
@@ -1,5 +1,68 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ejercicio1.registroAlumno.Alumno;
+import ejercicio1.registroAlumno.SistemaAlumnos;
+import ejercicio3.calculadoraSimple.Calculadora;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Ejercicio1 --> Registro de Alumnos
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el numero del ejercicio del cual quieres ver la solucion (1 a 4)");
+        int ejercicio = sc.nextInt();
+        mostrarEjercicio(ejercicio);
+        sc.close();
+
+    }
+
+    public static void mostrarEjercicio(int ejercicio) {
+
+        switch (ejercicio) {
+            case 1:
+                // Ejercicio1 --->Registro de alumnos
+                // ArrayList<Integer> calificaciones={1,2,3,4,5};
+                // int[] calificaciones = { 1, 5, 4, 6 };
+                // Alumno alumno1 = new Alumno("Daniel Bustos", 27, new int[] { 1, 5, 7, 9 });
+                // System.out.println(alumno1.getNombre());
+                // alumno1.setNombre("juan reyes");
+                // System.out.println(alumno1.getNombre());
+                // SistemaAlumnos sistemaAlumnos = new SistemaAlumnos();
+                // sistemaAlumnos.agregarAlumnos(alumno1);
+                // for (int i : alumno1.calificaciones) {
+                // System.out.println(i);
+                // }
+                // System.out.println(alumno1.calificaciones[2]);
+                // sistemaAlumnos.calcularPromedio(alumno1);
+                System.out.println("Holis 1");
+
+                break;
+            case 2:
+                // Ejercicio2 --->Conversor de monedas
+
+                break;
+
+            case 3:
+                // Ejercicio3 --->Calculadora simple
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Digite un numero: ");
+                float numeroUno = sc.nextFloat();
+                System.out.println("Digite un segundo numero: ");
+                float numeroDos = sc.nextFloat();
+                Calculadora calculadora = new Calculadora(numeroUno, numeroDos);
+                calculadora.mostrarResultados();
+                break;
+
+            case 4:
+                // Ejercicio4 ---> Sistema de reservas de hotel
+                System.out.println("Holis 4");
+                break;
+
+            default:
+                System.out.println("Debes digitar un numero entre 1 y 4");
+                break;
+        }
+
     }
 }
---
+++ src/ejercicio3/calculadoraSimple/Calculadora.java
@@ -0,0 +1,42 @@
+package ejercicio3.calculadoraSimple;
+
+public class Calculadora {
+    public float numeroUno;
+    public float numeroDos;
+
+    public Calculadora(float numeroUno, float numeroDos) {
+        this.numeroUno = numeroUno;
+        this.numeroDos = numeroDos;
+    }
+
+    public float suma(float numeroUno, float numeroDos) {
+        float suma = numeroUno + numeroDos;
+        return suma;
+    }
+
+    public float resta(float numeroUno, float numeroDos) {
+        float resta = numeroUno - numeroDos;
+        return resta;
+    }
+
+    public float multiplicacion(float numeroUno, float numeroDos) {
+        float multiplicacion = numeroUno * numeroDos;
+        return multiplicacion;
+    }
+
+    public float division(float numeroUno, float numeroDos) {
+        float division = numeroUno / numeroDos;
+        return division;
+    }
+
+    public void mostrarResultados() {
+        System.out.println(
+                "El resultado de la suma entre sus numeros es: " + suma(numeroUno, numeroDos) + "\n" +
+                        "El resultado de la resta entre sus numeros es: " + resta(numeroUno, numeroDos) + "\n" +
+                        "El resultado de la multiplicacion entre sus numeros es: "
+                        + multiplicacion(numeroUno, numeroDos) + "\n" +
+                        "El resultado de la division entre sus numeros es: " + division(numeroUno, numeroDos));
+
+    }
+
+}
