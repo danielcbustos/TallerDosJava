0ec84b30db31a5bfdb6301799750f7ac777c1b50
danielcbustos <d.bustosr@uniandes.edu.co>
2023-08-10 09:00:29 -0500

    menu conversor monedas mejorado

--- src/ejercicio2/conversorMonedas/ConversorMonedas.java
+++ src/ejercicio2/conversorMonedas/ConversorMonedas.java
@@ -1,11 +1,11 @@
 package ejercicio2.conversorMonedas;
 
 public class ConversorMonedas {
-    public String tipoMoneda;
+    public int tipoMoneda;
     public double cantidadDinero;
-    public String tipoMonedaFinal;
+    public int tipoMonedaFinal;
 
-    public ConversorMonedas(String tipoMoneda, double cantidadDinero, String tipoMonedaFinal) {
+    public ConversorMonedas(int tipoMoneda, double cantidadDinero, int tipoMonedaFinal) {
         this.tipoMoneda = tipoMoneda;
         this.cantidadDinero = cantidadDinero;
         this.tipoMonedaFinal = tipoMonedaFinal;
@@ -36,40 +36,95 @@ public class ConversorMonedas {
         System.out.println(cantidadDinero + " euros equivalen a " + conversionDinero + " pesos colombianos");
     }
 
-    public void euroDolares(double cantidadDinero) {
+    public void eurosDolares(double cantidadDinero) {
         double conversionDinero = cantidadDinero / 0.91;
         System.out.println(cantidadDinero + " euros equivalen a " + conversionDinero + " dolares");
     }
 
     public void tipoCantidadMoneda() {
+        // int monedaAconvertir = sc.nextInt();
+        switch (tipoMoneda) {
+            case 1:
+                switch (tipoMonedaFinal) {
+                    case 1:
+                        System.out.println("La moneda a convertir debe ser diferente a la moneda que vas a convertir");
+                        break;
+                    case 2:
+                        pesosDolares(cantidadDinero);
+                        break;
+                    case 3:
+                        pesosEuros(cantidadDinero);
+                        break;
 
-        if (tipoMoneda.equals("pesos")) {
-            if (tipoMonedaFinal.equals("dolares")) {
-                pesosDolares(cantidadDinero);
-            } else if (tipoMonedaFinal.equals("euros")) {
-                pesosEuros(cantidadDinero);
-            } else {
-                System.out.println("Escribe un tipo de moneda a convertir valido");
-            }
-        } else if (tipoMoneda.equals("dolares")) {
-            if (tipoMonedaFinal.equals("pesos")) {
-                dolaresPesos(cantidadDinero);
-            } else if (tipoMonedaFinal.equals("euros")) {
-                dolaresEuros(cantidadDinero);
-            } else {
-                System.out.println("Escribe un tipo de moneda a convertir valido");
-            }
-        } else if (tipoMoneda.equals("euros")) {
-            if (tipoMonedaFinal.equals("pesos")) {
-                eurosPesos(cantidadDinero);
-            } else if (tipoMonedaFinal.equals("dolares")) {
-                euroDolares(cantidadDinero);
-            } else {
-                System.out.println(tipoMonedaFinal + " no es un tipo de moneda valido");
-            }
-        } else {
-            System.out.println(tipoMoneda + " no es un tipo de moneda valido");
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                switch (tipoMonedaFinal) {
+                    case 1:
+                        dolaresPesos(cantidadDinero);
+                        break;
+                    case 2:
+                        System.out.println("La moneda a convertir debe ser diferente a la moneda que vas a convertir");
+                        break;
+                    case 3:
+                        dolaresEuros(cantidadDinero);
+                        break;
+
+                    default:
+                        break;
+                }
+                break;
+            case 3:
+                switch (tipoMonedaFinal) {
+                    case 1:
+                        eurosPesos(cantidadDinero);
+                        break;
+                    case 2:
+                        eurosDolares(cantidadDinero);
+                        break;
+                    case 3:
+                        System.out.println("La moneda a convertir debe ser diferente a la moneda que vas a convertir");
+                        break;
+
+                    default:
+                        System.out.println("Digita 1 o 2");
+                        break;
+                }
+                break;
+
+            default:
+                System.out.println("Digita un numero entre 1 y 3");
+                break;
         }
+        // if (tipoMoneda.equals("pesos")) {
+        // if (tipoMonedaFinal.equals("dolares")) {
+        // pesosDolares(cantidadDinero);
+        // } else if (tipoMonedaFinal.equals("euros")) {
+        // pesosEuros(cantidadDinero);
+        // } else {
+        // System.out.println("Escribe un tipo de moneda a convertir valido");
+        // }
+        // } else if (tipoMoneda.equals("dolares")) {
+        // if (tipoMonedaFinal.equals("pesos")) {
+        // dolaresPesos(cantidadDinero);
+        // } else if (tipoMonedaFinal.equals("euros")) {
+        // dolaresEuros(cantidadDinero);
+        // } else {
+        // System.out.println("Escribe un tipo de moneda a convertir valido");
+        // }
+        // } else if (tipoMoneda.equals("euros")) {
+        // if (tipoMonedaFinal.equals("pesos")) {
+        // eurosPesos(cantidadDinero);
+        // } else if (tipoMonedaFinal.equals("dolares")) {
+        // eurosDolares(cantidadDinero);
+        // } else {
+        // System.out.println(tipoMonedaFinal + " no es un tipo de moneda valido");
+        // }
+        // } else {
+        // System.out.println(tipoMoneda + " no es un tipo de moneda valido");
+        // }
     }
 
 }
